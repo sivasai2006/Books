1270d8485319be495a4a14e75fa1bd7e0f51e28f
Sivakumar Mahalingam <36898056+sivasai2006@users.noreply.github.com>
2019-05-14 17:13:04 +0530

    Update Streamer.py

--- A/Streamer.py
+++ A/Streamer.py
@@ -1,5 +1,6 @@
 # import the python subprocess module
 import subprocess
+import time
 
 def file_create():
     sourcefolder = "C:\\CodePlay\\"
@@ -14,6 +15,7 @@ def file_create():
             destfilepath = (destfolder+filename+"%d"%(i+1)+fileext)
             fwrite = open(destfilepath, "w+")
             fwrite.write(contents)
+            time.sleep(10)
 
             # Run Hadoop put command in Python
             (ret, out, err) = run_cmd(['hdfs', 'dfs', '-put', 'local_file', 'hdfs_file_path'])
@@ -23,7 +25,7 @@ def run_cmd(args_list):
     run linux commands
     """
     # import subprocess
-    print('Running system command: {0}'.format(' '.join(args_list)))
+    #print('Running system command: {0}'.format(' '.join(args_list)))
     proc = subprocess.Popen(args_list, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     s_output, s_err = proc.communicate()
     s_return = proc.returncode
